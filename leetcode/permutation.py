75c14c675dd4e0f5deaf87385dac663f3a2ca739
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-02-03 22:28:07 -0300

    first leetcode - problem for feb 4th

---
+++ leetcode/permutation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if s1 == s2 : return True
+        word = dict()
+        substrings = dict()
+        for c in s1:
+            word[c] = word.get(c, 0) + 1
+        for i in range(len(s2)):
+            c = s2[i]
+            substrings[c] = substrings.get(c, 0) + 1
+            if i >= len(s1):
+                c = s2[i - len(s1)]
+                n = substrings.pop(c) - 1
+                if n > 0: substrings[c] = n
+            if word == substrings: return True
+
+        return False
