deb0c7e241c9e44e157acf23301320ae87c3bad2
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-02-03 22:28:22 -0300

    forgot to add files

---
+++ leetcode/permutationtest.py
@@ -0,0 +1,15 @@
+import unittest
+import permutation
+
+class SolTest(unittest.TestCase):
+    def test1(self):
+        sol = permutation.Solution()
+        self.assertEqual(True, sol.checkInclusion("", ""))
+
+    def test2(self):
+        sol = permutation.Solution()
+        self.assertEqual(True, sol.checkInclusion("ab", "eidbaooo"))
+
+    def test3(self):
+        sol = permutation.Solution()
+        self.assertEqual(False, sol.checkInclusion(s1 = "ab", s2 = "eidboaoo"))
