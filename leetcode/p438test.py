a8aef591514168e8e88dcd85c03f90ec4662bfeb
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-02-04 21:39:28 -0300

    solution for p438

---
+++ leetcode/p438test.py
@@ -0,0 +1,16 @@
+import unittest
+from p438 import Solution
+
+
+class SolTest(unittest.TestCase):
+    def test1(self):
+        sol = Solution()
+        self.assertEqual([0, 6], sol.findAnagrams(s="cbaebabacd", p="abc"))
+
+    def test2(self):
+        sol = Solution()
+        self.assertEqual([0, 1, 2], sol.findAnagrams(s="abab", p="ab"))
+
+
+if __name__ == '__main__':
+    unittest.main()
