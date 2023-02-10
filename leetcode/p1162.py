afe65001ec471ec362f64ed229a694c52bbac76a
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-02-10 14:59:11 -0300

    done p1162, still pretty ugly

--- leetcode/p1162.py
+++ leetcode/p1162.py
@@ -19,25 +19,63 @@ class Solution:
                     memo[nx][ny] = 1
             return 0
 
-        distance = 1
-        queue = [(x, y, 1)]
+        distance = 0
+        queue = [(x, y, 0)]
+        visited = set()
         while len(queue) > 0:
             xx, yy, distance = queue.pop(0)
             if grid[xx][yy] == 1:
                 break
+            if memo[xx][yy] != -1:
+                distance = distance + memo[xx][yy]
+                break
+            if (xx, yy) in visited:
+                continue
+            visited.add((xx, yy))
             for direction in Solution.directions:
                 nx = xx + direction[0]
                 ny = yy + direction[1]
                 if nx in range(0, len(grid)) and ny in range(0, len(grid)):
                     queue.append((nx, ny, distance+1))
-        #
-        #         1
+        if len(queue) == 0:
+            return -1
+
         memo[x][y] = distance
         return distance
+
+    def maxDistance2(self, grid: List[List[int]]) -> int:
+        distances = [[-1 for _ in grid] for _ in grid]
+        queue = []
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                if grid[i][j] == 1:
+                    queue.append((i, j))
+        if len(queue) == 0 or len(queue) == len(grid) * len(grid):
+            return -1
+
+        dist = 0
+        while len(queue) > 0:
+            next_queue = []
+            while len(queue) > 0:
+                x, y = queue.pop()
+                if distances[x][y] != -1:
+                    continue
+                distances[x][y] = dist
+                for direction in Solution.directions:
+                    nx = x + direction[0]
+                    ny = y + direction[1]
+                    if nx in range(0, len(grid)) and ny in range(0, len(grid)) and distances[nx][ny] == -1:
+                        next_queue.append((nx, ny))
+            queue = next_queue
+            dist += 1
+        return max([max(x) for x in distances])
 
     def maxDistance(self, grid: List[List[int]]) -> int:
         distances = [[-1 for _ in grid] for _ in grid]
         for i in range(len(grid)):
             for j in range(len(grid)):
                 Solution.calc_distance(grid, i, j, distances)
-        return max([max(x) for x in distances])
+        r = max([max(x) for x in distances])
+        if r == 0:
+            return -1
+        return r
--- tests/p1162test.py
+++ tests/p1162test.py
@@ -4,11 +4,25 @@ from leetcode.p1162 import Solution
 
 class MyTestCase(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(2, Solution().maxDistance([[1, 0, 1], [0, 0, 0], [1, 0, 1]]))
+        self.assertEqual(2, Solution().maxDistance2([[1, 0, 1], [0, 0, 0], [1, 0, 1]]))
 
     def test_2(self):
-        self.assertEqual(4, Solution().maxDistance([[1, 0, 0], [0, 0, 0], [0, 0, 0]]))
+        self.assertEqual(4, Solution().maxDistance2([[1, 0, 0], [0, 0, 0], [0, 0, 0]]))
+
+    def test_3(self):
+        self.assertEqual(-1, Solution().maxDistance2([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+
+    def test_4(self):
+        self.assertEqual(-1, Solution().maxDistance2([[1, 1, 1], [1, 1, 1], [1, 1, 1]]))
+
+    def test_5(self):
+        self.assertEqual(2, Solution().maxDistance2(
+            [[1, 0, 0, 0, 0, 1, 0, 0, 0, 1], [1, 1, 0, 1, 1, 1, 0, 1, 1, 0], [0, 1, 1, 0, 1, 0, 0, 1, 0, 0],
+             [1, 0, 1, 0, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 1, 1, 0, 1, 1], [0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+             [0, 0, 0, 1, 1, 1, 1, 0, 0, 1], [0, 1, 0, 0, 1, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+             [1, 1, 0, 1, 1, 1, 1, 1, 0, 0]]))
 
 
+#
 if __name__ == '__main__':
     unittest.main()
