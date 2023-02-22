77f35f5b10c48c13567c608a6041baea91bd0687
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-02-22 16:12:16 -0300

    p1011

--- leetcode/p1011.py
+++ leetcode/p1011.py
@@ -2,14 +2,38 @@ from typing import List
 
 
 class Solution:
-    def shipWithinDays(self, weights: List[int], days: int) -> int:
-        from math import ceil
-        min_w = ceil(sum(weights) / days)
+
+    @staticmethod
+    def min_days(weights: List[int], max_w: int) -> int:
+        days = 0
         i = 0
-        for _ in range(days):
+        while i < len(weights):
             s = 0
-            while s < min_w and i < len(weights):
+            while i < len(weights) and s + weights[i] <= max_w :
                 s += weights[i]
                 i += 1
-            min_w = max(min_w, s)
+            if s == 0:
+                return 999999
+            days += 1
+        return days
+
+
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        min_w = 0
+        max_w = sum(weights)
+        while min_w < max_w:
+            mid_w = min_w + (max_w - min_w) // 2
+            d = Solution.min_days(weights, mid_w)
+            if d == days:
+                min_w = mid_w
+                break
+            elif d > days:
+                min_w = mid_w + 1
+            else:
+                max_w = mid_w - 1
+        while Solution.min_days(weights, min_w) <= days:
+            min_w -= 1
+        while Solution.min_days(weights, min_w) > days:
+            min_w += 1
         return min_w
+
