53c38c914ed8f25724c562d0060836130f28dc03
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-02-23 00:22:54 -0300

    p1011 not working

---
+++ leetcode/p502.py
@@ -0,0 +1,49 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        previous = [w for _ in range(len(profits))]
+        # need to keep track of what was used
+        used = set()
+        for _ in range(min(k, len(profits))):
+            current = previous.copy()
+            selected = -1
+            for i in range(len(profits)):
+                if i in used:
+                    continue
+                if previous[i] >= capital[i]:
+                    if i == 0 or (i > 0 and previous[i] + profits[i] > current[i-1]):
+                        selected = i
+                        current[i] = previous[i] + profits[i]
+                    else:
+                        current[i] = current[i-1]
+                elif i > 0:
+                    current[i] = current[i - 1]
+            previous = current
+            if selected == -1:
+                break
+            used.add(selected)
+        return previous[-1]
+
+    def findMaximizedCapital_1(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        max_found = 0
+        queue = deque()
+        for i in range(len(profits)):
+            if capital[i] <= w:
+                queue.append((w + profits[i], {i}))
+
+        while len(queue) > 0:
+            money, visited = queue.popleft()
+            max_found = max(max_found, money)
+            if len(visited) == k or len(visited) == len(profits):
+                max_found = max(max_found, money)
+            else:
+                for i in range(len(profits)):
+                    if i not in visited and capital[i] <= money:
+                        visited2 = visited.copy()
+                        visited2.add(i)
+                        queue.append((money + profits[i], visited2))
+        return max_found
